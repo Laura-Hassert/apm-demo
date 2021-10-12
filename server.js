0f0eef25320f6e58f81fc6d917c9712dc8857bd9
Laura-Hassert <laura.hassert@gmail.com>
2021-10-12 12:17:39 -0400

    fixed rollbar

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-// const Rollbar = require('rollbar');
+const Rollbar = require('rollbar');
+
 const rollbar = new Rollbar({
     accessToken: '272e2b755aa249349edfbc40b14bb878',
     captureUncaught: true,
