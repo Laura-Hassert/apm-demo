8e955dece17ad61cff6b7cbd3bac794c6f3d5067
Laura-Hassert <laura.hassert@gmail.com>
2021-10-12 12:00:39 -0400

    init commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+const port = process.env.port || 4400
+app.listen(port, () => console.log(`Server up and running on ${port}!`));
