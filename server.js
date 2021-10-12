6d0748a51cfb4c2cdd299d9559590afa39a4c8b8
Laura-Hassert <laura.hassert@gmail.com>
2021-10-12 12:32:49 -0400

    added middleware

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const rollbar = new Rollbar({
 
 const app = express();
 
+app.use(rollbar.errorHandler())
+
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
     rollbar.info('html file served successfully')
