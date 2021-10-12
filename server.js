930fddbfc19e713bf40296fd9e0f0368ac24abc6
Laura-Hassert <laura.hassert@gmail.com>
2021-10-12 12:05:54 -0400

    fixed PORT

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
-const port = process.env.port || 4400
+const port = process.env.PORT || 4400
 app.listen(port, () => console.log(`Server up and running on ${port}!`));
